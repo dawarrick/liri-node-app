28cfdeea1fb96d8c0d589bde367d91fe50ee2c8a
Deb <49885983+dawarrick@users.noreply.github.com>
2019-07-29 03:08:59 -0400

    all functionality working, writing to random log file

--- liri.js
+++ liri.js
@@ -14,32 +14,32 @@ var spotify = new Spotify(keys.spotify);
 var results = [];
 var command = "";
 
+//these are the valid commands they can run
+var validCommand = ["concert-this", "spotify-this-song", "movie-this", "do-what-it-says"];
+
 //get command line parameters
 //this should be the action the user wants to perform
 if (process.argv[2] !== undefined) {
     command = process.argv[2].toLowerCase();
 }
 
-//this is the lookup value
+//this is the lookup value if passed
 var searchCriteria = process.argv.splice(3).join(" ");
 
+//separate screen displays
 var divider = "*--------------------------------------------------------------------------------------------------------*"
 
 
-//make sure they entered a valid command.  If not, let's prompt them to select from a list
+//Spotify if spotify-this-song is passed.
 function spotifyCall() {
-    //Spotify if spotify-this-song is passed.
 
-    console.log("spotify")
     //'The Sign' will be the default if spotify called with no song 
-    if (searchCriteria === "") {
+    if (searchCriteria === "" || searchCriteria === null || searchCriteria === undefined) {
         searchCriteria = "The Sign";
     }
     results = [];
     spotify.search({ type: 'track', query: searchCriteria, limit: 1 })
         .then(function (response) {
-            //console.log(response.tracks)
-            //console.log(response.tracks.album)
             response.tracks.items.forEach(function (ea) {
                 results.push({
                     artist: ea.artists,
@@ -52,8 +52,8 @@ function spotifyCall() {
                 noResults()
             }
             else {
-            printSpotify();         //send to the console
-            writeToRandom();        //add to text file of commands
+                printSpotify();         //send to the console
+                writeToRandom();        //add to text file of commands
             }
         })
         .catch(function (err) {
@@ -73,7 +73,7 @@ function concert() {
     }
     axios.get(`https://rest.bandsintown.com/artists/${searchCriteria}/events?app_id=codingbootcamp?date=upcoming`)
         .then(function (response) {
-            //console.log(response.data);
+            //console.log("response.data "+response.data);
             response.data.forEach(function (ea) {
 
                 results.push({
@@ -112,29 +112,29 @@ function concert() {
         });
 }
 
-
-
-/* movie-this will pull from the OMDB database
-    if no movie is passed, it will display for the movie 'Mr. Nobody'
-*/
-
+// movie-this will pull from the OMDB database
 function movie() {
 
     results = [];
-    //if nothing passed in going to default to Madonna
+    // if no movie is passed, it will display for the movie 'Mr. Nobody'
     if (searchCriteria === "") {
         searchCriteria = "Mr. Nobody";
     }
 
     axios.get(`http://www.omdbapi.com/?t=${searchCriteria}&y=&plot=short&apikey=trilogy`).then(
         function (response) {
-            //console.log(JSON.stringify(response.data));
+
+            //rotten rating does not always exist
+            var rottenRating1 = 'N/A';
+            if (response.data.Ratings.findIndex(i => i.Source === "Rotten Tomatoes") > -1) {
+                rottenRating1 = response.data.Ratings[response.data.Ratings.findIndex(i => i.Source === "Rotten Tomatoes")].Value
+            }
 
             results.push({
                 title: response.data.Title,
                 year: response.data.year,
                 imdbRating: response.data.imdbRating,
-                rottenRating: response.data.Ratings[response.data.Ratings.findIndex(i => i.Source === "Rotten Tomatoes")].Value,
+                rottenRating: rottenRating1,
                 country: response.data.Country,
                 language: response.data.language,
                 plot: response.data.Plot,
@@ -153,8 +153,8 @@ function movie() {
 
 //if the user want the program to select, will select a randomly from random.txt and call the process in the table
 function doRandom() {
-    //console.log("Doing Random")
-    // load an array of the values from random.txt
+ 
+    // load an array of the values from previously select list (random.txt)
     // The code will store the contents of the reading inside the variable "data"
     fs.readFile("random.txt", "utf8", function (error, data) {
 
@@ -164,25 +164,33 @@ function doRandom() {
         }
 
         //get rid of the line feeds and put into an array
-        var regex = /\r\n/;
+        var regex = /\r\n/gi;
         newData = data.replace(regex, ',').split(",");
 
         //each entry is in the array not each pair, so need to get an even number back to make sure we get the command first
-        i = getIndex(newData.length / 2);
+        var i = getIndex(newData.length);
+        if (i > 0 && i % 2 !== 0) {
+            i--;
+        }
+        
         command = newData[i];
-        regex = /"/;
-        searchCriteria = newData[i + 1];  //.replace(regex, '');
+        //need to get rid of the quotes on the search
+        regex = /"/gi;
+        searchCriteria = newData[i + 1].replace(regex, "");
+        //now execute the selected command
+        processCommand();
 
-        processInput()
     });
 
 }
 
-//function will return the array value for a random command
+//function will return the array value for a random command in random.txt
 function getIndex(arrayLength) {
-    return Math.floor(Math.random() * (arrayLength - 1)) * 2;  //need to multiply by 2 to get an even index
+    return Math.floor(Math.random() * (arrayLength - 1));
 }
 
+//print to screen Spotify information
+//I have limited return to 1, but am leaving the functionality to show multiple in case wanted in the future.
 function printSpotify() {
     for (var i = 0; i < results.length; i++) {
         //need to loop through the artists because there can be multiple
@@ -213,24 +221,23 @@ function printSpotify() {
     }
 }
 
+//print to screen the concerr 
 function printConcert() {
-
+    //need to loop through the concerts because there can be multiple
     for (var i = 0; i < results.length; i++) {
-        //need to loop through the artists because there can be multiple
         console.log(divider)
+        console.log("Band: " + searchCriteria);
         console.log("Venue: " + results[i].venue)
         console.log("Location: " + results[i].location)
         console.log("Date: " + results[i].date)
-        console.log(divider)
     }
-
-
+    console.log(divider)
 }
 
-
+//print to screen the movie information
 function printMovie() {
+    //need to loop through the movies because there can be multiple
     for (var i = 0; i < results.length; i++) {
-        //need to loop through the artists because there can be multiple
         console.log(divider)
         console.log("Title: " + results[i].title)
         console.log("Year: " + results[i].year)
@@ -248,6 +255,7 @@ function printMovie() {
 
 }
 
+//if nothing was found that matches, let them know
 function noResults() {
     console.log(divider);
     console.log("No criteria was found that matched your search " + searchCriteria)
@@ -267,12 +275,11 @@ function convertDate(dateIn, dateFormatIn, dateFormatOut) {
     return convertedDate = moment(dateIn, dateFormatIn).format(dateFormatOut);
 };
 
-
-
+//will evaluate what was passed in.
 function processInput() {
     // If user didn't enter a command, or entered an invalid one, prompt for one
-    var commandTilde = command + '~';
-    if (command === "" || !commandTilde.indexOf("spotify-this-song~concert-this~movie-this~do-what-it-says~")) {
+
+    if (command === "" || !validCommand.includes(command)) {
 
         inquirer.prompt([
             {
@@ -282,20 +289,23 @@ function processInput() {
                 choices: ["Spotify", "Bands in Town", "Movies", "Pick for me", "Quit"]
             },
             {
+              //only prompt for the search if one of the first three options
+                when: function (response) {
+                    return (response.doingWhat !== "Pick for me" && response.doingWhat !== "Quit");
+                },
                 type: "input",
                 name: "searchValue",
                 message: "What would you like to search for?"
             }
-
-
         ]).then(function (input) {
 
             searchCriteria = input.searchValue;
             console.log("do what " + input.doingWhat)
+            console.log("searchCriteria " + input.searchCriteria)
 
             //get a random selection from those previously
             if (input.doingWhat === "Pick for me") {
-                //console.log("do what " + input.doingWhat)
+                searchCriteria = "";
                 command = 'do-what-it-says'
             }
             // convert selection to command
@@ -310,28 +320,24 @@ function processInput() {
             }
             //quit
             else {
-                console.log("return")
                 return;
             }
-            main();
+            processCommand();
         });
     }
     else {
-        main();
+        processCommand();
     }
 }
 
-function main() {
-    console.log("command " + command)
-    console.log("search " + searchCriteria)
+//once the input is validated, will process the command
+function processCommand() {   
 
     //need to get a random command to execute
     if (command === 'do-what-it-says') {
         doRandom();
-        console.log("command2 " + command)
     }
-
-    if (command === "movie-this") {
+    else if (command === "movie-this") {
         movie();
     }
     else if (command === "concert-this") {
@@ -341,83 +347,8 @@ function main() {
         spotifyCall();
     }
     else {
-        console.log("command " + command)
+        console.log("command not found " + command)
     }
 }
 
-
 processInput();
-
-//var found = arr.filter(function(item) { return item.name === 'k1'; });
-//Source":"Rotten Tomatoes","Value":"73%"
-
-/*axios.get(`http://www.omdbapi.com/?t=remember+the+titans&y=&plot=short&apikey=trilogy`)
-    .then(function (response) {
-        // If the axios was successful...
-        // Then log the body from the site!
-        console.log(response.data);
-        console.log("Name of Venue: " + JSON.stringify(data[0].venue.name))
-        console.log("Location: " + JSON.stringify(data[0].venue.city + ", " + data[0].venue.region))
-        console.log("Date of Event (MM/DD/YYYY): " + JSON.stringify(data[0].datetime))
-
-    })
-    .catch(function (error) {
-        if (error.response) {
-            // The request was made and the server responded with a status code
-            // that falls out of the range of 2xx
-            console.log(error.response.data);
-            console.log(error.response.status);
-            console.log(error.response.headers);
-        } else if (error.request) {
-            // The request was made but no response was received
-            // `error.request` is an object that comes back with details pertaining to the error that occurred.
-            console.log(error.request);
-        } else {
-            // Something happened in setting up the request that triggered an Error
-            console.log("Error", error.message);
-        }
-        console.log(error.config);
-    });
-*/
-
-
-
-//https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp?date=upcoming
-
-
-/*This will search the Bands in Town Artist Events API (`"https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp"`)
-
-//* You should then be able to access your keys information like so
-//pull in the spotify keys
-
-  var spotify = new Spotify(keys.spotify);
-
-
-9. Make it so liri.js can take in one of the following commands:
-
-/*(bandsintown)
-node liri.js concert-this <artist/band name here>
-
-(spotify)
-`node liri.js spotify-this-song '<song name here>'`
-   * `spotify-this-song`
-(ODBM) `node liri.js movie-this '<movie name here>'`
-   * `movie-this`
-
-   * `do-what-it-says`
-   /*Hey everyone I found this nice bit of code that allows you to use the readline module that comes default with
-   node The readline allows you to prompt the user for an input. This snippet in particular solves the tamagotchi
-   problem and prevents the file itself from closing, so you can keep inputting commands without having the values reset.*/
-/*   const readline = require('readline');
-   const rl = readline.createInterface({
-       input: process.stdin,
-       output: process.stdout
-   });
-   var repeat = function () {
-       rl.question('Input an action... ', (input) => {
-           input = input.split(" ");
-           animals[input[0]][input[1]]();
-           repeat();
-       });
-   }
-   repeat();*/
